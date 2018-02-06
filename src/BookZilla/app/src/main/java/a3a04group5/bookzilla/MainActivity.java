804b5c7beccffd2c1a6deefd7378828c4c83ed9a
Michael <panuntom@mcmaster.ca>
2018-02-06 10:09:49 -0500

    Android Project Initialized

---
+++ src/BookZilla/app/src/main/java/a3a04group5/bookzilla/MainActivity.java
@@ -0,0 +1,13 @@
+package a3a04group5.bookzilla;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
