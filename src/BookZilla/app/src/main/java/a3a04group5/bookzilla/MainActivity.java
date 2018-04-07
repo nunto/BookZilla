7af3e4b3560d991c693ee3a8c28605ffa01bb6f6
Michael <panuntom@mcmaster.ca>
2018-04-07 16:36:23 -0400

    MainActivity started -- Just basic homepage layout and whatnot, still incomplete

--- src/BookZilla/app/src/main/java/a3a04group5/bookzilla/MainActivity.java
+++ src/BookZilla/app/src/main/java/a3a04group5/bookzilla/MainActivity.java
@@ -1,7 +1,10 @@
 package a3a04group5.bookzilla;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button goSearch = (Button) findViewById(R.id.btnSearch);
+
+
+        goSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SearchEntry.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
