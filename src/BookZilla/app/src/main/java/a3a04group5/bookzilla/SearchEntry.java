f88691fb5ebdd1e07057bfa68c5c63fb04f719fc
MohamedBengezi <bengezimohamed@gmail.com>
2018-04-08 15:34:46 -0400

    Instantiated input fields for use when conducting search

--- src/BookZilla/app/src/main/java/a3a04group5/bookzilla/SearchEntry.java
+++ src/BookZilla/app/src/main/java/a3a04group5/bookzilla/SearchEntry.java
@@ -2,12 +2,56 @@ package a3a04group5.bookzilla;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class SearchEntry extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class SearchEntry extends AppCompatActivity implements View.OnClickListener {
+
+    private Button search_button;
+    //private TextView output;
+    private EditText author, publisher, publishedDate, category, description;
+    String text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_entry);
+
+        search_button = (Button) findViewById(R.id.search_button);
+        author = (EditText) findViewById(R.id.author);
+        publisher = (EditText) findViewById(R.id.publisher);
+        publishedDate = (EditText) findViewById(R.id.publishedDate);
+        category = (EditText) findViewById(R.id.category);
+        description = (EditText) findViewById(R.id.description);
+
+        //output = (TextView) findViewById(R.id.attributes_title);
+
+        search_button.setOnClickListener(this);
+    }
+
+    public void onClick(View v){
+        /*THIS IS TO TEST WHETHER THE INPUT FIELDS DISPLAY CORRECTLY
+        if (v.getId() == search_button.getId()){
+            text = author.getText().toString();
+            output.setText(text);
+        }
+        */
+
+
+    }
+
+
+    public void buttonOnClick(View v) {
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.TOP| Gravity.LEFT, 0, 0) ;
+        toast.makeText(SearchEntry.this, author.getText(), toast.LENGTH_SHORT).show();
+
     }
 }
